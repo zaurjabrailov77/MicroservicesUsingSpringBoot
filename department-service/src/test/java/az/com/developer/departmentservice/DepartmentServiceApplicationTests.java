9cf16ccdc71bbac4a6f97d860e9b294910661f1c
zaurjabrailov77 <66160217+zaurjabrailov77@users.noreply.github.com>
2021-04-23 18:25:53 +0400

    add department service

---
+++ department-service/src/test/java/az/com/developer/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.com.developer.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
