4c570911d348a5f079ba775c33e005d0759be319
zaurjabrailov77 <66160217+zaurjabrailov77@users.noreply.github.com>
2021-05-03 11:07:55 +0400

    Finish User Service and start new poject Cloud Gateway

---
+++ service-registry/service-registry/src/main/java/az/com/developer/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package az.com.developer.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+		System.out.println("Started Eureka Server");
+	}
+}
