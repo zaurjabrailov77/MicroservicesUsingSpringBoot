3c3fc0289d85396271c63dfa4344374fdef785b2
zaurjabrailov77 <66160217+zaurjabrailov77@users.noreply.github.com>
2021-05-16 10:46:19 +0400

    Cloud Config Server

--- cloud-config-server/cloud-config-server/src/main/java/az/com/developer/cloudconfigserver/CloudConfigServerApplication.java
+++ cloud-config-server/cloud-config-server/src/main/java/az/com/developer/cloudconfigserver/CloudConfigServerApplication.java
@@ -2,12 +2,17 @@ package az.com.developer.cloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class CloudConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigServerApplication.class, args);
+		System.out.println("Started Cloud Config Server");
 	}
 
 }
